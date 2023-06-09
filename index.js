78e3982b5309ca795439840747956a2a1efcd27d
Sydney Laub <syd.laub18@gmail.com>
2023-06-09 08:48:14 -0400

    wrote function to prompt the user to answer questions about their logo using inquirer

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// import inquirer
+const inquirer = reuquire('inquirer');
+
+// import file system module
+const fs = require('fs');
+
+
 // GIVEN a command - line application that accepts user input
+
+
 // WHEN I am prompted for text
 // THEN I can enter up to three characters
 // WHEN I am prompted for the text color
@@ -7,6 +16,39 @@
 // THEN I am presented with a list of shapes to choose from: circle, triangle, and square
 // WHEN I am prompted for the shape's color
 // THEN I can enter a color keyword(OR a hexadecimal number)
+
+//function to promp the user to answer questions about their logo and save the users answers
+function promptUser() {
+    inquirer
+        .prompt([
+            // text for logo
+            {
+                type:"input",
+                message:"What text would you like the logo to display? Please only enter three characters.",
+                name:"text",
+            },
+            // text color
+            {
+                type: "input",
+                message: "Input desired text color using a color keyword or a hexidecimal number.",
+                name: "textColor",
+            },
+            // shape of logo
+            {
+                type: "input",
+                message: "What shape would you like the logo to display?",
+                choices: ["Triangle", "Square", "Circle"],
+                name: "shape",
+            },
+            // shape color
+            {
+                type: "input",
+                message: "Input desired shape background color using a color keyword or a hexidecimal number.",
+                name: "shapeColor",
+            },
+        ])
+}
+
 // WHEN I have entered input for all the prompts
 // THEN an SVG file is created named`logo.svg`
 // AND the output text "Generated logo.svg" is printed in the command line
